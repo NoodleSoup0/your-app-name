f7b3aaad0f14c31e36cf1017b6c7ffc34fb5fe45
Linh Ly <linhly2025@u.northwestern.edu>
2024-09-26 01:23:55 -0500

    Fetch Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,49 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch.js';
 
+const queryClient = new QueryClient();
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
+const Main = ({ setData, setLoading, setError }) => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  // Update the states 
+  React.useEffect(() => {
+    setData(data);
+    setLoading(isLoading);
+    setError(error);
+  }, [data, isLoading, error, setData, setLoading, setError]);
+
+  if (error) return <h1>Error loading course data: {error.message}</h1>;
+  if (isLoading) return <h1>Loading course data...</h1>;
+
+  return null; 
 };
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Banner title={schedule.title} />
-        <CourseList courses={schedule.courses} />
-        {/* <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p> */}
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          
+
+          <Main setData={setData} setLoading={setLoading} setError={setError} />
+
+          {isLoading && <h1>Loading courses...</h1>}
+          {error && <h1>Error loading courses: {error.message}</h1>}
+          {data && <Banner title={data.title}/>}
+          {data && <CourseList courses={data.courses} /> }
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 };
 
 export default App;
-
