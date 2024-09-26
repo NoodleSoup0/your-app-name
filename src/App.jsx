282684b1b94e16d7490854f214c65f6d59ae0e8e
Linh Ly <linhly2025@u.northwestern.edu>
2024-09-25 22:56:29 -0500

    Initial Schedule Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// const schedule = {
-//   title: "CS Courses for 2018-2019"
-// };
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <h1>{schedule.title}</h1> */}
+        <h1>{schedule.title}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
